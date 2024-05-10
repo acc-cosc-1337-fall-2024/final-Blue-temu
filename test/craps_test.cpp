84c229a670a847d1308c50cfbfbb238b8a40a0bc
Blen-yoseph <157077852+Blue-temu@users.noreply.github.com>
2024-05-10 18:59:14 +0000

    Question 4

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,9 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "phase.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -45,5 +48,53 @@ TEST_CASE("Verify Test simulate shooting dice ") {
 
 	REQUIRE(value >= 2);
 	REQUIRE(value <= 12);
+    }
+}
+
+
+TEST_CASE("Verify Test ComeOutPhaseTest ")
+{
+    ComeOutPhase comeOutPhase;
+    Die die1, die2;
+
+   for (int i = 0; i < 10; ++i) {
+        Roll* roll = new Roll(die1, die2); 
+        comeOutPhase.get_outcome(roll);
+        roll->roll_dice(); 
+        int rolled_value = roll->roll_value(); 
+
+        if (rolled_value == 7 || rolled_value == 11) {
+            REQUIRE(comeOutPhase.get_outcome(roll) == RollOutcome::natural);
+
+        } else if (rolled_value == 2 || rolled_value == 3 || rolled_value == 12) {
+            REQUIRE(comeOutPhase.get_outcome(roll) == RollOutcome::craps);
+            
+        } else {
+            REQUIRE(comeOutPhase.get_outcome(roll) == RollOutcome::point);
+        }
+    }
+}
+
+TEST_CASE("Verify Test PointPhaseTest ")
+{
+    int point = 8;
+    PointPhase pointPhase(point);
+    Die die1, die2;
+
+   for (int i = 0; i < 10; ++i) {
+        Roll* roll = new Roll(die1, die2); 
+        pointPhase.get_outcome(roll);
+        roll->roll_dice(); 
+        int rolled_value = roll->roll_value(); 
+
+        if (rolled_value == point) {
+            REQUIRE(pointPhase.get_outcome(roll) == RollOutcome::point);
+
+        } else if (rolled_value == 7) {
+            REQUIRE(pointPhase.get_outcome(roll) == RollOutcome::seven_out);
+            
+        } else {
+            REQUIRE(pointPhase.get_outcome(roll) == RollOutcome::nopoint);
+        }
     }
 }
